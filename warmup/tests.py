361f886ce3284c8cd49150c0b09966bdb243927c
Pei Jun Chen <pjc.shadowed@berkeley.edu>
2013-02-15 16:36:58 -0800

    update 5

--- warmup/tests.py
+++ warmup/tests.py
@@ -5,12 +5,20 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+#from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+#class SimpleTest(TestCase):
+#    def test_basic_addition(self):
+#        """
+#        Tests that 1 + 1 always equals 2.
+#        """
+#        self.assertEqual(1 + 1, 2)
+
+import os
+from os.path import abspath, dirname
+import sys
+
+project_dir = abspath(dirname(dirname(__file__)))
+sys.path.insert(0, project_dir)
+os.environ['DJANGO_SETTINGS_MODULS'] = "LoginCounter.settings"
